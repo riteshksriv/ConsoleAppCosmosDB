fb6c07982acca07339959d6dcae0e59feacda7d6
Ritesh Srivastava <riteshkumar@microsoft.com>
2023-11-29 17:26:43 +0530

    Init repo

---
+++ ConsoleAppCosmosDB/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Diagnostics;
+using Microsoft.Azure.Cosmos;
+
+namespace ConsoleAppCosmosDB
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Debug.WriteLine("Hello, World!");
+            await Task.FromResult("Hello World!");
+        }
+
+        public static async Task Run(string connectionString)
+        {
+            var clientOptions = new CosmosClientOptions
+            {
+                ConnectionMode = ConnectionMode.Gateway,
+                HttpClientFactory = () =>
+                {
+                    HttpMessageHandler httpMessageHandler = new HttpClientHandler()
+                    {
+                        ServerCertificateCustomValidationCallback = HttpClientHandler.DangerousAcceptAnyServerCertificateValidator
+                    };
+                    return new HttpClient(httpMessageHandler);
+                }
+            };
+            var cosmosClient = new CosmosClient(connectionString, clientOptions);
+            var dbResponse = await cosmosClient.CreateDatabaseIfNotExistsAsync("MyDatabase");
+            await dbResponse.Database.CreateContainerIfNotExistsAsync("MyTable", "/id");
+        }
+    }
+}
---
+++ IntegrationTests/UnitTest1.cs
@@ -0,0 +1,42 @@
+using System.Diagnostics;
+using ConsoleAppCosmosDB;
+using DotNet.Testcontainers.Builders;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Testcontainers.CosmosDb;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        const string COSMOSDB_IMAGE = "mcr.microsoft.com/cosmosdb/linux/azure-cosmos-emulator:latest";
+        static string connString;
+        static CosmosDbContainer cosmosDb;
+
+        [ClassInitialize]
+        public static async Task ClassInitialize(TestContext context)
+        {
+            var cosmosDb = new ContainerBuilder()
+                .WithImage(COSMOSDB_IMAGE)
+                .WithPortBinding(8087, true)
+                .Build();
+            await Task.Delay(1000);
+            await cosmosDb.StartAsync();
+            int port = cosmosDb.GetMappedPublicPort(8087);
+            Debug.WriteLine($"Port number = {port}");
+        }
+
+        [ClassCleanup]
+        public static async Task ClassCleanup()
+        {
+            await cosmosDb.StopAsync();
+        }
+
+        [TestMethod]
+        public async Task TestMethod1()
+        {
+            await Program.Run(connString);
+            Assert.IsTrue(true);
+        }
+    }
+}
